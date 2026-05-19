e8f57658687196945a3e75eee353686166af70a6
engghazwani1989 <myghazwani@uj.edu.sa>
2026-05-19 05:36:12 +0300

    Add console logs for function type checks
    Added console logs to check function types.

--- script (4).js
+++ script (4).js
@@ -5,3 +5,6 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
+// Open browser console and run:
+console.log(typeof loadInitialDataWithCloud); // Should be 'function'
+console.log(typeof syncNowWithCloud); // Should be 'function'
